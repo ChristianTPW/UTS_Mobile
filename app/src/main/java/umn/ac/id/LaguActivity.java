63cb71f70731dc537f15af5b2fb41e71100aab85
Christian Widjaja <christianwidjaja04@gmail.com>
2021-03-21 16:00:49 +0700

    Add External Storage Permission

--- app/src/main/java/umn/ac/id/LaguActivity.java
+++ app/src/main/java/umn/ac/id/LaguActivity.java
@@ -3,15 +3,30 @@ package umn.ac.id;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.LinkedList;
 
 public class LaguActivity extends AppCompatActivity {
+    private final LinkedList<String> mDaftarLagu = new LinkedList<>();
+
+    private static final int PERMISSION_REQ = 1;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,8 +56,6 @@ public class LaguActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lagu);
 
-
-
         AlertDialog alertDialog = new AlertDialog.Builder(LaguActivity.this).create();
         alertDialog.setTitle("Selamat Datang");
         alertDialog.setMessage("Christian Teguh Prasetya Widjaja\n00000032062");
@@ -53,5 +66,55 @@ public class LaguActivity extends AppCompatActivity {
                     }
                 });
         alertDialog.show();
+
+        if (ContextCompat.checkSelfPermission(LaguActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(LaguActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(LaguActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQ);
+            }else{
+                ActivityCompat.requestPermissions(LaguActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQ);}
+
+        }else{
+            getMusic();
+        }
+
+
+
+
+    }
+
+    public void getMusic(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri songuri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor song = contentResolver.query(songuri, null, null, null, null);
+
+        if(song != null && song.moveToFirst()){
+            int songTitle = song.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songArtist = song.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+            do{
+                String currTitle = song.getString(songTitle);
+                mDaftarLagu.add(currTitle);
+                Log.e("myTag", currTitle);
+            }while (song.moveToNext());
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_REQ:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    if(ContextCompat.checkSelfPermission(LaguActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
